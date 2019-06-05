d8b4db2b728b26822f0de96c28a6c9c0eab8532c
raghav1408 <1408.raghavsharma@gmail.com>
2019-06-06 01:50:00 +0530

    Added test post method

--- src/profiles_project/profile_api/views.py
+++ src/profiles_project/profile_api/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from . import serializers
+from rest_framework import status
 # Create your views here.
 
 class HelloApiView(APIView):
     """Class for testing our API"""
-
+    serializer_class = serializers.HelloViewSerializer
     def get(self, request , format = None):
         """ rest framework get function """
         an_apiview = [
@@ -16,3 +19,14 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message' : 'Test API','an api view': an_apiview})
+
+    def post(self,request):
+        """ post function for rest framework """
+        serializer = serializers.HelloViewSerializer(data = request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            email = serializer.data.get('email')
+            message = "Hello "+ str(name) + " your email is "+str(email)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
